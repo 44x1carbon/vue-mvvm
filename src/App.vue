2bd12b9582c541489274e4e8661a0974d70d67bf
44x1carbon <b4079@oic.jp>
2019-03-06 00:52:30 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <div>
+        <label for="">商品名</label>
+        <input type="text" v-model="itemName">
+      </div>
+      <div>
+        <label for="">金額(税抜き)</label>
+        <input type="number" v-model.number="itemPrice">円
+      </div>
+      <button @click="addItem">商品を追加</button>    
+    </div>
+    <table>
+      <tr>
+        <th>商品名</th><th>金額(税抜き)</th><th>金額(税込み)</th>
+      </tr>
+      <tr v-for="item in items" :key="item.itemName">
+        <td>{{ item.itemName }}</td>
+        <td>{{ item.itemPrice }}円</td>
+        <td>{{ item.itemPrice * 1.08 }}円</td>
+      </tr>      
+      <tr>
+        <td>送料</td>
+        <td>{{ carriage }}</td>
+      </tr>
+      <tr>
+        <td>合計金額</td>
+        <td>{{totalPlace}}円</td>
+        <td>{{totalPlace * 1.08}}円</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+const DEFAULR_CARRIAGE = 300;
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      itemName: "",
+      itemPrice: 0,
+      items: [],
+      
+    }
+  },
+  methods: {
+    addItem() {
+      this.items.push({
+        itemName: this.itemName,
+        itemPrice: this.itemPrice
+      })
+    }
+  },
+  computed: {
+    itemTotalPlace() {
+      return this.items.reduce((p, c) => {         
+        return p + c.itemPrice;
+      }, 0);
+    },
+    totalPlace() {
+      return this.itemTotalPlace + this.carriage;
+    },
+    carriage() {
+      return this.itemTotalPlace >= 3000 ? 0 : DEFAULR_CARRIAGE;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
